7e3692b4216f2195f3515b3710700ea8bd946abe
Daniel Gonzalez <daanielgr8@gmail.com>
2024-06-28 20:05:55 +0200

    conditionals

--- 08-conditionals.py
+++ 08-conditionals.py
@@ -1,2 +1,29 @@
 ### Conditionals ###
 
+my_condition = False
+
+if my_condition:
+    print("The first IF is true")
+
+my_condition = 5 * 2
+
+if my_condition > 10 and my_condition < 20:
+    print("greater than 10 and less than 20")
+else: # else doesnt need a condition
+    print("lesser than 10 or greater or equal than 20")
+
+if my_condition > 10 and my_condition < 20:
+    print("greater than 10 and less than 20")
+elif my_condition == 10: # elif needs a condition
+    print("equal to 10")
+
+my_string = "My string"
+
+if my_string:
+    print("The string is not empty")
+
+if not my_string:
+    print("The string is empty")
+
+if my_string == "My string":
+    print("The strings are equal")
