0e220faa901a79ffc9b24f686085997f7cd1a2cf
Daniel Gonzalez <daanielgr8@gmail.com>
2024-07-01 22:59:31 +0200

    list comprehension lesson

--- Intermediate/01_list_comprehension.py
+++ Intermediate/01_list_comprehension.py
@@ -1,9 +1,12 @@
 ### List Comprehension ###
 
-my_original_list = [0, 1, 2, 3, 4, 5]
-
-my_list = [i for i in range(81)] # 
-print(my_list)
+my_original_list = [0, 1, 2, 3, 4, 5] 
 
 my_range = range(8)
 print(list(my_range))
+
+my_list = [i for i in range(8)] # 0 to 7
+print(my_list)
+ 
+my_list = [i + 1 for i in range(8)] # 1 to 8 because i + 1
+print(my_list)
